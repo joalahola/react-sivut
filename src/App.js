4ebf9d7e9cd38061d5f0dad24673d5760eb5a616
joalahola <85728207+joalahola@users.noreply.github.com>
2021-12-30 18:49:17 +0200

    testi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState, useEffect } from 'react'
+import CoinGecko from 'coingecko-api'
+import Filter from './components/Filter'
+import InputField from './components/InputField'
+
+
+
+const App = () => {
+
+  const [data, setData]= useState([]) 
+  const [startDate, setStartDate]= useState("2020-01-01") // vakiopaivamaara
+  const [EndDate, setEndDate]= useState("2020-12-31")     // vakiopaivamaara
+  
+
+
+/* Tapahtumankasittelija ensimmaiselle inputfieldille */
+const handleFilterChange = (Event) => {
+  setStartDate(Event.target.value)
 }
 
-export default App;
+/* Tapahtumankasittelija toiselle inputfieldille */
+const handleEndChange = (Event) => {
+  setEndDate(Event.target.value)
+}
+
+   
+useEffect(() => {
+  const CoinGeckoClient = new CoinGecko();
+ 
+  let loppu = new Date().getTime()  
+  
+  CoinGeckoClient
+  .coins.fetchMarketChartRange('bitcoin', {
+    vs_currency: 'eur',
+    from: 0 ,
+    to: loppu + 3600
+  })
+  .then(response => {
+   
+    setData(response.data)
+  })
+}, [])
+
+  
+
+return (
+  <div> 
+    <h1>
+      A small work
+    </h1>
+   <h2>Here you can see some information about the price of bitcoin</h2>
+    <div>Please input start and end date</div>
+  <InputField value = {startDate} onChange ={handleFilterChange} description = {'start date'}/>
+  <InputField value = {EndDate} onChange ={handleEndChange} description = {'end date'}/>
+  <Filter startDate = {startDate} endDate= {EndDate} array = {data} />
+  </div>
+)
+
+}
+export default App
